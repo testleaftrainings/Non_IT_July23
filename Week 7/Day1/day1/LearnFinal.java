75a204db784cf0a15772de04ea21c3d670c0a867
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2023-09-23 22:07:20 +0530

    Add files via upload

---
+++ Week 7/Day1/day1/LearnFinal.java
@@ -0,0 +1,18 @@
+package week7.day1;
+
+public final class LearnFinal {
+	
+	static final int cvv=123;//variable
+	String name;
+	
+	public final void accountInfo() {
+		System.out.println("Account details");
+	}
+
+	public static void main(String[] args) {
+		//LearnFinal  obj =new LearnFinal ();
+		System.out.println(cvv);
+
+	}
+
+}
---
+++ Week 7/Day1/day1/RBI.java
@@ -0,0 +1,5 @@
+package week7.day1;
+
+public interface RBI {
+
+}
