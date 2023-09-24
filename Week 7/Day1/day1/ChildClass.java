f665da38ba89936e215fb3bcb19b86c71cfc4bcc
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2023-09-24 23:56:12 +0530

    Delete Week 7 directory

--- Week 7/Day1/day1/ChildClass.java
+++
@@ -1,7 +0,0 @@
-package week7.day1;
-
-public class ChildClass extends LearnFinal {
-	public void accountInfo() {
-		System.out.println("Account details");
-	}
-}
--- Week 7/Day1/day1/RBI.java
+++
@@ -1,5 +0,0 @@
-package week7.day1;
-
-public interface RBI {
-
-}
